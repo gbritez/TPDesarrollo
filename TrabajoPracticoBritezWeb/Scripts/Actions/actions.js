3155734af3c9a23f526dfc147e1ccdfd14542b79
Gonzalo Britez <gonzalo_britez@hotmail.com>
2019-07-02 09:28:13 -0300

    Updates

---
+++ TrabajoPracticoBritezWeb/Scripts/Actions/actions.js
@@ -0,0 +1,12 @@
+﻿function goToAdd() {
+    location.href = "Add.html";
+}
+function goToEdit() {
+    location.href = "Edit.html";
+}
+function goToWatch() {
+    location.href = "Watch.html";
+}
+function logOff() {
+    location.href = "Login.html";
+}
